461b56e4893e183b8e682f4a293cb5477e7029d1
frankiesastronautfund <grotton.2@buckeyemail.osu.edu>
2019-07-03 17:36:33 -0400

    added billing and client side app

---
+++ client/src/components/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+import Landing from "./Landing";
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+const Dashboard = () => {
+  return <h2>dashboard</h2>;
+};
+
+const SurveyNew = () => {
+  return <h2>surveynew</h2>;
+};
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(App);
